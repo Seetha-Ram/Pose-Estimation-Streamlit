8a6912305635cdce2b29b8fedf7fc3f724c6eb37
Coming Soon <seetharambethala121@gmail.com>
2023-09-25 17:43:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,59 @@
+import streamlit as st
 import cv2
 import mediapipe as mp
 import numpy as np
-import streamlit as st
+import tempfile
+import os
 
-# Initialize MediaPipe Pose solution
-mp_pose = mp.solutions.pose
-mp_draw = mp.solutions.drawing_utils
-pose = mp_pose.Pose()
-
-# Initialize video capture
-cap = None
-
-# Function to generate frames
-def generate_frames():
-    global cap
-    while cap is not None:
-        ret, img = cap.read()
-        if not ret:
-            break
-
-        # Resize image/frame
-        img = cv2.resize(img, (600, 400))
-
-        # Do Pose detection
-        results = pose.process(img)
-        # Draw the detected pose on the video frame
-        mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                               mp_draw.DrawingSpec((255, 0, 0), 2, 2),
-                               mp_draw.DrawingSpec((255, 0, 255), 2, 2))
-
-        # Extract and draw pose on a plain white image
-        h, w, c = img.shape
-        opImg = np.zeros([h, w, c])
-        opImg.fill(255)
-
-        # Draw extracted pose on a black and white image
-        mp_draw.draw_landmarks(opImg, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                               mp_draw.DrawingSpec((255, 0, 0), 2, 2),
-                               mp_draw.DrawingSpec((255, 0, 255), 2, 2))
-
-        # Display pose on the original video/live stream
-        st.image(img, channels="BGR", use_column_width=True, caption="Pose Estimation")
-
-        # Display extracted pose on a blank image
-        st.image(opImg, channels="BGR", use_column_width=True, caption="Extracted Pose")
-
-# Streamlit app
 def main():
-    global cap
+    st.title("Pose Estimation with OpenCV and MediaPipe")
 
-    st.title("Pose Estimation App")
+    uploaded_file = st.file_uploader("Choose a video file", type=["mp4"])
 
-    st.sidebar.title("Upload Video")
-    uploaded_file = st.sidebar.file_uploader("Choose a video...", type=["mp4"])
-    
-    if uploaded_file:
-        cap = cv2.VideoCapture(uploaded_file)
-    
-    if st.sidebar.button("Start Pose Estimation"):
-        st.write("Processing...")
-        generate_frames()
+    if uploaded_file is not None:
+        # Create a temporary directory to store the uploaded video
+        temp_dir = tempfile.mkdtemp()
+        temp_file_path = os.path.join(temp_dir, "temp_video.mp4")
 
-if __name__ == '__main__':
+        # Save the uploaded file to the temporary directory
+        with open(temp_file_path, "wb") as temp_file:
+            temp_file.write(uploaded_file.read())
+
+        # Initialize MediaPipe Pose solution
+        mp_pose = mp.solutions.pose
+        mp_draw = mp.solutions.drawing_utils
+        pose = mp_pose.Pose()
+
+        # Open the video file with OpenCV
+        cap = cv2.VideoCapture(temp_file_path)
+
+        if not cap.isOpened():
+            st.error("Error: Could not open video file.")
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Resize the frame
+            frame = cv2.resize(frame, (600, 400))
+
+            # Perform pose estimation on the frame
+            results = pose.process(frame)
+
+            # Draw the detected pose on the video frame
+            mp_draw.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                   mp_draw.DrawingSpec((255, 0, 0), 2, 2),
+                                   mp_draw.DrawingSpec((255, 0, 255), 2, 2))
+
+            # Display the frame with annotated pose
+            st.image(frame, channels="BGR")
+
+        cap.release()
+
+        # Remove the temporary directory and file
+        os.remove(temp_file_path)
+        os.rmdir(temp_dir)
+
+if __name__ == "__main__":
     main()
